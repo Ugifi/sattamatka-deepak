73c39f41afce78fa8ebfbfa9de33626c24379805
Ugifi <79358347+Ugifi@users.noreply.github.com>
2026-08-25 14:45:13 +0530

    Delete frontend directory

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.matka.king',
-  appName: 'YONO MATKA',
-  webDir: 'build'
-};
-
-export default config;
